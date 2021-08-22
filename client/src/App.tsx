9e1cfb42dc1f542782a5e010fd16a23ccab06832
Kim Möller <kim.moller2@gmail.com>
2021-08-22 11:59:29 +0300

    Init React client app
    Init React client app with Typescript and Vite builder

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Component } from "react";
+
+interface State { }
+
+interface Props { }
+
+export default class App extends Component {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Book Collection</h1>
+      </div>
+    )
+  }
+}
