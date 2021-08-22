85c13a28647abf3696eac5295ecee849d66f4b33
Kim Möller <kim.moller2@gmail.com>
2021-08-22 15:14:28 +0300

    Render list of requested book titles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,42 @@
 import React from "react";
 import { Component } from "react";
+import { Book, fetchBookCollection } from "./BookService";
 
-interface State { }
+interface State {
+  books: Book[]
+}
 
 interface Props { }
 
-export default class App extends Component {
+export default class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  getBookCollection() {
+    fetchBookCollection().then(books => {
+      this.setState({
+        books: books
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getBookCollection();
   }
 
   render() {
     return (
       <div>
         <h1>Book Collection</h1>
+        <ul>
+          {this.state.books.map(book => {
+            return <li>{book.title}</li>
+          })}
+        </ul>
       </div>
     )
   }
